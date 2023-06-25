076910f611621a007435b9e00ce212e0495c516d
moonieew <90208509+moonieew@users.noreply.github.com>
2023-06-25 11:46:24 +0000

    Deploying to gh-pages from @ moonieew/moonStudify@4def10f18542628a043b73e16d7b2fbf99c7280a 🚀

--- src/app/rootReducer.ts
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-
-const rootReducer = combineReducers({});
-
-export type RootReducer = ReturnType<typeof rootReducer>;
-
-export default rootReducer;
--- src/pages/class/member.tsx
+++
@@ -1,9 +0,0 @@
-import Member from "@/views/Member";
-
-function MemberPage() {
-    return (
-        <Member />
-    );
-}
-
-export default MemberPage;
