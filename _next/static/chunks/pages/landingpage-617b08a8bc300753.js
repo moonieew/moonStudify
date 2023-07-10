bab7f3a607671f99d14088fdde8adb438228ed0a
moonieew <90208509+moonieew@users.noreply.github.com>
2023-07-10 16:38:42 +0000

    Deploying to gh-pages from @ moonieew/moonStudify@9e20bc9875470809469163fb8c3fcc82cb5dc880 🚀

---
+++ _next/static/chunks/pages/landingpage-617b08a8bc300753.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[894],{7689:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/landingpage",function(){return u(6792)}])}},function(n){n.O(0,[792,774,888,179],function(){return n(n.s=7689)}),_N_E=n.O()}]);
