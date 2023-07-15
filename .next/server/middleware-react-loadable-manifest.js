de3656959f62b1ac7210ac27997e84e207bbe094
moonieew <nguyet.le@kyanon.digital>
2023-07-15 12:15:11 +0700

    create post and cmt

--- .next/server/middleware-react-loadable-manifest.js
+++ .next/server/middleware-react-loadable-manifest.js
@@ -1,1 +1,1 @@
-self.__REACT_LOADABLE_MANIFEST={}
+self.__REACT_LOADABLE_MANIFEST={"../views/Newsfeed/index.tsx -> react-quill":{"id":"../views/Newsfeed/index.tsx -> react-quill","files":["static/chunks/node_modules_react-quill_lib_index_js.js"]}}
